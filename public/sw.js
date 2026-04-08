892556c8c2aa94fcda27a6b866b7be907bdef196
Print-sp <gestaodefrotaprint@gmail.com>
2026-04-07 23:04:59 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // Service Worker - Controle de Patio Print
 // Desenvolvido por Ramalho Sistemas e Software
 
-const CACHE_NAME = 'patio-print-v5.7-seminovos';
+const CACHE_NAME = 'patio-print-v5.8-seminovos-edit';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
