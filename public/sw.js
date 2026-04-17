ce1466b53135c742f34c0f399f7f4b5bf7d3cb50
Print-sp <gestaodefrotaprint@gmail.com>
2026-04-17 07:42:00 -0300

    Update cache name for service worker

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // Service Worker - Controle de Patio Print
 // Desenvolvido por Ramalho Sistemas e Software
 
-const CACHE_NAME = 'patio-print-v5.8-seminovos-edit';
+const CACHE_NAME = 'patio-print-v6.0-seminovos-vendido';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
